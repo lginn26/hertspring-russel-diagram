7cb6d9bcfc2ca4faaf74d575cd7ff7cbe685fdad
Louis Godfrey <lginn26@users.noreply.github.com>
2021-04-27 20:18:40 -0400

    updated star_list, redid color calculations for star, added display logic, added new font, removed plot_star.ogg

--- hertsrping_russel_diagram.py
+++ hertsrping_russel_diagram.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 from star import Star
 
@@ -6,6 +7,9 @@ pygame.init()
 
 # Images
 base_image = pygame.image.load("assets/base_diagram.jpg")
+
+# Fonts
+font1 = pygame.font.Font("assets/SpaceGrotesk-VariableFont_wght.ttf", 20)
 
 # Stages
 START = 0
@@ -39,12 +43,44 @@ YELLOW = (255, 255, 0)
 GREEN = (100, 255, 100)
 SKYBLUE = (0, 238, 255)
 
+# Functions
+def display_message(message):
+    star_txt = font1.render(message, 1, RED)
+    w = star_txt.get_width()
+    screen.blit(star_txt, [0, 950])
+
 # Star List
 star_list = [
-   Star("Betelgeuse", "M2", 2.06, -5.85, 150000, 3500, 20),
-   Star("Antares", "M1", 1.83, -5.28, 1.6, 18500, 7.2),
-   Star("Proxima Centauri", "M5.5", 1.82, 11.05, .0017, 3042, .123),
-   Star("Van Maanen's Star", "DZ8", .546, 12.374, .00016,6220, 0.633)
+   Star("The Sun",	"G2V",0.63,4.83,1,5700,1),
+   Star("Achernar",	"B3V",	-0.16,	-2.77,	3150,15000,6.7),
+   Star("Acrux","B1V",-0.26,-3.77,25000,24000,15.52),
+   Star("Adhara",	"B2 II",-0.21,-4.8,38700,22900,12.6),
+   Star("Aldebaran",	"K5+ III",	1.44,	-2.1,	518,	3910,	1.7),
+   Star("Alpha Centauri",	"G2V",	0.71,	4.38,	1.519,	5790,	1.1),
+   Star("Altair",	"A7 V",	0.09,	2.22,	10.6,	7700,	1.79),
+   Star("Antares",	"M1",	1.83,	-5.28,	1.6,	18500,	7.2),
+   Star("Arcturus",	"K0 III",	1.23,	-0.04,	170,	4290,	1.1),
+   Star("Barnard's Star",	"M4",	1.713,	9.511,	0.0004,	3134,	144),
+   Star("Betelgeuse",	"M2",	2.06,	-5.85,	150000,	3500,	20),
+   Star("Canopus",	"A9 II",	0.1,	-5.71,	107000,	7400,	9.8),
+   Star("Capella",	"G3 III",	0.44,	0.08,	77.6,	4940,	2.69),
+   Star("Castor",	"A1V",	0.03,	0.986,	34,	10286,	2.76),
+   Star("Deneb",	"A2",	0.23,	-8.38,	196000,	8525,	19),
+   Star("Formalhaut",	"A3V",	0.08,	1.72,	16.63,	8590,	1.92),
+   Star("Gacrux",	"M3",	1.59,	-0.52,	820,	3626,	1.3),
+   Star("Hadar",	"B1VIII",	-0.23,	-5.42,	41700,	25500,	4.61),
+   Star("Pollux",	"K0III",	1,	1.07,	43,	4865,	2.04),
+   Star("Polaris",	"F7Ib",	0.01,	-3.62,	1260,	6015,	6.5),
+   Star("Procyon B",	"DQZ",	0.42,	10.7,	0.00055,	7740,	0.602),
+   Star("Proxima Centauri",	"M5.5",	1.82,	11.05,	0.0017,	3042,	0.123),
+   Star("Regulus",	"B7V",	-0.11,	-0.52,	316.2,	12460,	3.5),
+   Star("Rigel",	"B8Ia",	-0.06,	-6.72,	66000,	11000,	17),
+   Star("Shaula",	"B2IV",	-0.240,	-5.05,	36300,	25000,	11),
+   Star("Sirius",	"A1",	-0.03,	-1.46,	25.4,	9940,	2.02),
+   Star("Spica",	"B1",	-0.23,	-3.55,	12100,	22399,	10.3),
+   Star("Van Maanen's Star",	"DZ8",	0.546,	12.374,	0.00016,	6220,	0.633),
+   Star("Vega",	"A0",	0,	0.03,	50,	9602,	2.135),
+   Star("Wolf 359",	"M6",	2.034,	13.54,	0.0014,	2800,	0.09)
 
 ]
 
@@ -68,10 +104,20 @@ while not done:
     screen.blit(base_image, [0,0])
 
     # Displays each instance of Star in star_list
+    displayed_star = None
+
+    message = "Hover close to the center of a star to reveal its data!"
     for star in star_list:
-        star.display(screen, WHITE)
-    
-    print(pygame.mouse.get_pos())
+        star.display(screen)
+
+        distx = abs(pygame.mouse.get_pos()[0] - star.posx)
+        disty = abs(pygame.mouse.get_pos()[1] - star.posy)
+        distance = math.sqrt( math.pow(distx, 2) +  math.pow(disty, 2))
+
+        if (distance < star.radius):
+            message = "{}: class:{}, color index:{}, absolute magnitude:{}, luminosity:{} SL, surface tempature:{} K, mass:{} SM".format(star.name, star.classification, star.color_index, star.absolute_magnatiude, star.luminosity, star.surface_tempature, star.mass)
+
+    display_message(message) 
 
     # Update screen (Draw the picture in the window.)
     pygame.display.flip()
--- star.py
+++ star.py
@@ -25,6 +25,7 @@ lum_step = 0
 temp_size_sf = 20/1192
 lum_size_sf = 20/860
 
+color_scheme_barrier = 10000
 
 class Star(pygame.sprite.Sprite):
 
@@ -49,7 +50,31 @@ class Star(pygame.sprite.Sprite):
         # Object Data
         self.posx = origin_x - (x_step * (surface_tempature / temp_step))
         self.posy = origin_y - (math.log10(luminosity) * y_step)
+        self.radius = (temp_size_sf * self.posx) + ((lum_size_sf * (1192 - self.posy)))
 
-    def display(self, surface, color):
-        radius = (temp_size_sf * self.posx) + ((lum_size_sf * (1192 - self.posy)))
-        pygame.draw.circle(surface, color, (self.posx, self.posy), radius)
+    def display(self, surface):
+
+        # Calculate Color
+        star_type = self.classification[0:1]
+        if (star_type == "O"):
+            color = [0, 110, 255]
+            color2 = [0, 90, 235]
+        elif (star_type == "B"):
+            color = [99, 167, 255]
+            color2 = [79, 80, 225]
+        elif (star_type == "A" or star_type == "F"):
+            color = [255, 255, 255]
+            color2 = [235, 235, 235]
+        elif (star_type == "G"):
+            color = [255, 255, 0]
+            color2 = [235, 235, 0]
+        elif (star_type == "K"):
+            color = [255, 136, 0]
+            color2 = [235, 116, 0]
+        else:
+            color = [255, 0, 0]
+            color2 = [235, 0, 0]
+
+        # Draw Star
+        pygame.draw.circle(surface, color2, (self.posx, self.posy), self.radius)
+        pygame.draw.circle(surface, color, (self.posx, self.posy), self.radius-2)
